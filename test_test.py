9ec662eedb72c7bfc6b88c87a3d01c92ca7583c4
Kamiel Braet <kamiel.braet@gmail.com>
2023-04-23 18:26:19 +0200

    Commit all remaining files.

---
+++ test_test.py
@@ -0,0 +1,13 @@
+import test    # The code to test
+import unittest   # The test framework
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(test.increment(3), 4)
+
+    # This test is designed to fail for demonstration purposes.
+    def test_decrement(self):
+        self.assertEqual(test.decrement(3), 2)
+
+if __name__ == '__main__':
+    unittest.main()
